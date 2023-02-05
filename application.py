bac0255f6086cf22dd6a418b1bc17a80a441487b
koishi755 <koishi755@gmail.com>
2023-02-06 01:10:49 +0900

    test1

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>Hello koishi</h1></body></html>\n"
+    return "<html><body><h1>Hello koishitest</h1></body></html>\n"
